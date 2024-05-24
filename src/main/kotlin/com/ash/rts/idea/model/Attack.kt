328f8ed29f72f7594f1d4fa35b8de2cc77a0fae0
Ash French <ashfrench@users.noreply.github.com>
2024-05-24 10:14:55 +0100

    Create the notion of a merged attack and defense

--- src/main/kotlin/com/ash/rts/idea/model/Attack.kt
+++ src/main/kotlin/com/ash/rts/idea/model/Attack.kt
@@ -1,7 +1,15 @@
 package com.ash.rts.idea.model
 
+import com.ash.rts.ideal.com.ash.rts.idea.model.RtsUnit
+
 interface Attack {
 
     fun availableAttacks(): Set<HitBox>
+
+}
+
+interface MergedAttack: Attack {
+
+    fun mergedUnits(): Collection<RtsUnit>
 
 }
--- src/main/kotlin/com/ash/rts/idea/model/Defense.kt
+++ src/main/kotlin/com/ash/rts/idea/model/Defense.kt
@@ -1,7 +1,15 @@
 package com.ash.rts.idea.model
 
+import com.ash.rts.ideal.com.ash.rts.idea.model.RtsUnit
+
 interface Defense {
 
     val hitBox: HitBox
+
+}
+
+interface MergedDefense : Defense {
+
+    fun mergedUnits(): Collection<RtsUnit>
 
 }
